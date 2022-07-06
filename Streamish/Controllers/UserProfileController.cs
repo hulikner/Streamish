83d45d672411c8d7cde02473ab8e681e27fb6175
Hulikner <hulikner@gmail.com>
2022-07-06 13:43:08 -0500

    init

---
+++ Streamish/Controllers/UserProfileController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Streamish.Models;
+using Streamish.Repositories;
+using Microsoft.AspNetCore.Authorization;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Streamish.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _profileRepo;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _profileRepo = userProfileRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_profileRepo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var userProfile = _profileRepo.GetById(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            _profileRepo.Add(userProfile);
+            return CreatedAtAction("Get", new { id = userProfile.Id }, userProfile);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return BadRequest();
+            }
+
+            _profileRepo.Update(userProfile);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _profileRepo.Delete(id);
+            return NoContent();
+        }
+
+        [HttpGet("GetByIdWithVideos/{id}")]
+        public IActionResult GetByIdWithVideos(int id)
+        {
+            var profile = _profileRepo.GetByIdWithVideos(id);
+            return Ok(profile);
+        }
+    }
+}
---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Streamish.Models;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+         List<Video> GetAll();
+         Video GetById(int id);
+         void Add(Video video);
+         void Update(Video video);
+         void Delete(int id);
+         List<Video> GetAllWithComments();
+         Video GetVideoByIdWithComments(int id);
+    }
+}
